6cb036fdaf74b308c7e78aae3bd750389162d9df
TimGels <43609220+TimGels@users.noreply.github.com>
2021-03-31 15:24:35 +0200

    added remaining comments

--- src/mementos/HeaterMemento.java
+++ src/mementos/HeaterMemento.java
@@ -3,17 +3,28 @@ package mementos;
 import actuators.Heater;
 import actuators.HeaterModus;
 
+/**
+ * The HeaterMemento represents a saved state of the Heater actuator. 
+ */
 public class HeaterMemento implements Memento
 {
 	private Heater originator;
 	private HeaterModus state;
 
+	/**
+	 * Constructor of the HeaterMemento.
+	 * @param originator
+	 * @param state
+	 */
 	public HeaterMemento(Heater originator, HeaterModus state)
 	{
 		this.originator = originator;
 		this.state = state;
 	}
 
+	/**
+	 * Restores the last state of the heater.
+	 */
 	@Override
 	public void restore()
 	{
